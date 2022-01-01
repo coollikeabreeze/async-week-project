8f1a98dd373268122e4765a7d33e047ba1834605
Andrea Khan <khan.andrea@gmail.com>
2022-01-01 12:22:58 -0500

    removed redux folder from app directory

--- app/main.js
+++ app/main.js
@@ -2,18 +2,33 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './store';
 import css from '../public/style.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import Routers from './components/Routers'
 
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
 
 ReactDOM.render(
   <Router>
-  <Provider store={store}>
+  <ApolloProvider client={client}>
     <Routers />
-  </Provider>
+  </ApolloProvider>
   </Router>,
   document.getElementById('app')
 );
--- app/redux/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import dummyReducer from './reducer'
-
-const appReducer = combineReducers({
-  //add/update reducer states here:
-  dummyReducer: dummyReducer
-})
-
-export default appReducer
